c550b84c1c92203308c54e200ca415b7ce4d80cd
HIND-HANSALI <hindhsl111@gmail.com>
2023-12-15 16:05:45 +0100

    add Competition model

---
+++ src/app/models/Competition.ts
@@ -0,0 +1,11 @@
+export interface Competition {
+    id: number;
+    code: string;
+    date: Date;
+    startTime: { hours: number, minutes: number };
+    endTime: { hours: number, minutes: number };
+    totalMember: number;
+    location: string;
+    amount: number;
+   
+  }
