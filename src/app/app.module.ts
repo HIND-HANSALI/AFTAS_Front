ff82f406e70b61252bf2f8b565c23436df8b5b6b
HIND-HANSALI <hindhsl111@gmail.com>
2023-12-15 16:28:53 +0100

    implement other components in appcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CompetitionComponent } from './components/competition/competition.component';
+import { FishComponent } from './components/fish/fish.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ListCompetitionComponent } from './components/competition/list-competition/list-competition.component';
+import { AddCompetitionComponent } from './components/competition/add-competition/add-competition.component';
+import { UpdateCompetitionComponent } from './components/competition/update-competition/update-competition.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompetitionComponent
+   
+    FishComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ListCompetitionComponent,
+    AddCompetitionComponent,
+    UpdateCompetitionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
