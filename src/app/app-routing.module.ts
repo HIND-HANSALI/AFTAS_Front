5374f37fe428b189a5b880408ddb7ecbe9d90d04
HIND-HANSALI <hindhsl111@gmail.com>
2023-12-15 15:57:34 +0100

    configure routing of competition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { ListCompetitionComponent } from './components/competition/list-competition/list-competition.component';
+import { AddCompetitionComponent } from './components/competition/add-competition/add-competition.component';
+import { UpdateCompetitionComponent } from './components/competition/update-competition/update-competition.component';
+
+const routes: Routes = [
+  { path: 'competitions', component: ListCompetitionComponent },
+  { path: '', component: NavbarComponent },
+  { path: 'competitions/create', component: AddCompetitionComponent },
+  { path: 'competitions/update', component: UpdateCompetitionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
