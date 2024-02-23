ed9a0d618e30e8631527fe14b2bea88dc3e2a487
HIND-HANSALI <hindhsl111@gmail.com>
2024-02-23 08:22:14 +0100

    add condition for sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 
 @Component({
@@ -12,5 +13,17 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     initFlowbite();
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showSidebar = !(event.url.includes('login') || event.url.includes('register')) // Hide sidebar on login page
+        console.log('Show sidebar:', this.showSidebar);
+      }
+    });
   }
+
+  showSidebar: boolean = true;
+
+  constructor(private router: Router) { }
+
+
 }
